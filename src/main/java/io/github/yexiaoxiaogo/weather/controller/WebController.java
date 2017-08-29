04b444eb7237d8565a7279d73231dcfc57005c3d
yexiaoxiaogo <841079936@qq.com>
2017-08-29 21:36:17 +0800

    历史记录在web显示

--- src/main/java/io/github/yexiaoxiaogo/weather/controller/WebController.java
+++ src/main/java/io/github/yexiaoxiaogo/weather/controller/WebController.java
@@ -1,22 +1,22 @@
 package io.github.yexiaoxiaogo.weather.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import io.github.yexiaoxiaogo.weather.service.WeatherService;
+
 @Controller
 public class WebController {
 
-    @RequestMapping("/hello111")
-    @ResponseBody
-    public String hello111() {
-        return "Hello World";
-    }
     
+	@Autowired
+	private WeatherService weatherService;
 	@RequestMapping("/")
 	public String index(ModelMap map){
-		map.addAttribute("host","http://localhost:8080/weather/list/city?city=杭州");
+		map.addAttribute("weatherlist",weatherService.findWeatherListByName("杭州"));
 		return "index";
 	}
 }
--- src/main/resources/templates/index.ftl
+++ src/main/resources/templates/index.ftl
@@ -1,15 +1,31 @@
 <!DOCTYPE html>
-    <html xmlns="http://www.w3.org/1999/xhtml" 
-    xmlns:th="http://www.thymeleaf.org" 
-    xmlns:sec="http://www.thymeleaf.org/thymeleaf-extras-springsecurity3"
-    xmlns:layout="http://www.ultraq.net.nz/thymeleaf/layout">
+<html>
 
 <head>
-<meta charset="UTF-8">
+<meta charset="UTF-8" />
 <title>杭州天气历史记录</title>
 </head>
 <body>
-FreeMarker模板引擎
-<h1>${host} </h1>
+	<h1>杭州市天气历史记录</h1>
+	<table border="1">
+		<tr>
+			<td>城市</td>
+			<td>白天天气</td>
+			<td>夜间天气</td>
+			<td>最高温</td>
+			<td>最低温</td>
+			<td>日期</td>
+		</tr>
+		<#list weatherlist as weather>
+			<tr>
+				<td> ${weather.city} </td>
+				<td> ${weather.txt_d} </td>
+				<td> ${weather.txt_n} </td>
+				<td> ${weather.max} </td>
+				<td> ${weather.min} </td>
+				<td> ${weather.date} </td>
+			</tr>
+		</#list>
+	</table>
 </body>
 </html>
